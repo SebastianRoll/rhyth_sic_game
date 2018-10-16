fb26a433c8fa1f1be0731fbdcef77dd1f7d28708
Sebastian Roll <sebastian.roll@webstep.no>
2018-10-16 18:00:59 +0200

    Add boot

--- rhyth_game/boot.py
+++ rhyth_game/boot.py
@@ -1,11 +1,49 @@
-# from machine.Neopixel import RED, BLUE, YELLOW, GREEN, PURPLE
+# connect to a WIFI AP, sync time and start telnet and FTP server
+# Paste it in the Python command line (REPL)
+# Then you can connect to the IP of your ESP32 in FTP (passive mode) or in telnet !
 
-# import network
+wifi_ssid = "HackheimWifi"
+wifi_passwd = "TeletextFTW"
 
-# sta_if = network.WLAN(network.STA_IF)
-# print('connecting to network...')
-# sta_if.active(True)
-# sta_if.connect('Work-Work by ITsjefen', '')
-# while not sta_if.isconnected():
-#     pass
-# print('network config:', sta_if.ifconfig())
+import network
+import utime
+import machine
+
+
+def wifi(ssid="HackheimWifi", pwd="TeletextFTW"):
+    sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+    sta_if.connect(ssid, pwd)
+
+    tmo = 50
+    while not sta_if.isconnected():
+        utime.sleep_ms(100)
+        tmo -= 1
+        if tmo == 0:
+            break
+    if tmo > 0:
+        ifcfg = sta_if.ifconfig()
+        print("WiFi started, IP:", ifcfg[0])
+        utime.sleep_ms(500)
+
+        rtc = machine.RTC()
+        print("Synchronize time from NTP server ...")
+        rtc.ntp_sync(server="hr.pool.ntp.org")
+        tmo = 100
+        while not rtc.synced():
+            utime.sleep_ms(100)
+            tmo -= 1
+            if tmo == 0:
+                break
+
+        if tmo > 0:
+            utime.sleep_ms(200)
+            print("Time set:", utime.strftime("%c"))
+            print("")
+
+
+def ftp(user="micro", password="python"):
+    network.ftp.start(user=user, password=password, buffsize=1024, timeout=300)
+
+
+def telnet():
+    network.telnet.start(user="micro", password="python", timeout=300)
