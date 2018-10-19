5f2abc15290e87a66c39a49fbaed60b37b1dc0d4
Sebastian Roll <sebastian.roll@webstep.no>
2018-10-19 23:53:09 +0200

    Change pin definitions

--- rhyth_game/pin_definitions.py
+++ rhyth_game/pin_definitions.py
@@ -1,14 +1,7 @@
-pin_mosi = 23
-pin_miso = 19
-pin_sck = 18
-pin_slave_select_red = 17
-pin_slave_select_green = 16
-pin_button_red_1 = 35
-pin_button_red_2 = 34
 pin_uart2_rx = 32
 pin_uart2_tx = 25
-pin_ws2812 = 26
+pin_ws2812 = 21
 pin_ws2813 = 22
-pin_outer = 5
+pin_outer = 19
 
 touch_pins = [15, 33, 4, 13]#, 27, 12, 0, 2]
