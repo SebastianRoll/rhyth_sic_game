d55a972f897be1c090311c9b5a9976a26163bd57
Sebastian Roll <sebastian.roll@webstep.no>
2018-10-19 22:24:20 +0200

    Add memory check func to boot script

--- rhyth_game/boot.py
+++ rhyth_game/boot.py
@@ -8,6 +8,8 @@ wifi_passwd = "TeletextFTW"
 import network
 import utime
 import machine
+import gc
+import os
 
 
 def wifi(ssid="HackheimWifi", pwd="TeletextFTW"):
@@ -47,3 +49,18 @@ def ftp(user="micro", password="python"):
 
 def telnet():
     network.telnet.start(user="micro", password="python", timeout=300)
+
+
+def df():
+  s = os.statvfs('')
+  return ('{0} MB'.format((s[0]*s[3])/1048576))
+
+
+def free(full=False):
+  gc.collect()
+  F = gc.mem_free()
+  A = gc.mem_alloc()
+  T = F+A
+  P = '{0:.2f}%'.format(F/T*100)
+  if not full: return P
+  else : return ('Total:{0} Free:{1} ({2})'.format(T,F,P))
